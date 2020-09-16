72acc16277f57388daf2ce5de7f56e47c618435a
ab-anand <abhinavanand1905@gmail.com>
2020-09-16 11:12:46 +0530

    frequencyHeap unittest done

--- tests/test_frequencyHeap.py
+++ tests/test_frequencyHeap.py
@@ -1,8 +1,28 @@
 import unittest
+import sys
+from filezen.frequencyHeap import frequencyheap
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+sys.path.insert(0, '..')
+
+
+class TestFrequencyHeap(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.heap = frequencyheap.MaxFrequency()
+        self.heap.appendAddress("home/Documents/")
+        self.heap.appendAddress("home/Documents/")
+        self.heap.appendAddress("home/Downloads/")
+
+    def test_getMaxOccurringAddress(self):
+        result = self.heap.getMaxOccurringAddress
+        correct_answer = "home/Documents/"
+        self.assertEqual(result, correct_answer)
+
+    def test_getValueList(self):
+        result = self.heap.getValueList
+        correct_answer = ["home/Documents/", "home/Documents/", "home/Downloads/"]
+        self.assertEqual(result, correct_answer)
+
 
 if __name__ == '__main__':
     unittest.main()
