02c2bb9da07da5a430d48d5e9b117a2529454fbb
ab-anand <abhinavanand1905@gmail.com>
2020-09-19 09:35:38 +0530

    advanced scanner unittests done

--- filezen/version.py
+++ filezen/version.py
@@ -1,2 +1,2 @@
 # Version of Filezen package
-VERSION = "1.2.0"
+VERSION = "1.2.1"
--- tests/test_advancedScanner.py
+++ tests/test_advancedScanner.py
@@ -1,10 +1,39 @@
 import unittest
+import os
+import sys
+import shutil
+
+from filezen.advancedScanner import advancedscanner
+
+sys.path.insert(0, '..')
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+class TestAdvancedScanner(unittest.TestCase):
+    def setUp(self):
+        self.advancedscanner = advancedscanner.AdvancedScanner()
+        cwd = os.getcwd()
+        test_directory = "scantest"
+        path = os.path.join(cwd, test_directory)
+        if not os.path.exists(path):
+            os.mkdir(path)
+        self.test_dir_path = path
 
+    def test_setOutputPath(self):
+        self.advancedscanner.setOutputPath(self.test_dir_path)
+        result = self.advancedscanner.outputPath
 
+        self.assertEqual(self.test_dir_path, result)
+    
+    def test_setDepth(self):
+        depth = 10
+        self.advancedscanner.setDepth(depth)
+        result = self.advancedscanner.depth
+        
+        self.assertEqual(depth, result)
+    
+    def tearDown(self):
+        shutil.rmtree(self.test_dir_path, ignore_errors=True)
+
+    
 if __name__ == '__main__':
     unittest.main()
